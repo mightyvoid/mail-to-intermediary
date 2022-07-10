a35dcdbbe49d238a063628ad88be8ef156bfc0dd
Navin <navin@Navins-MacBook-Air.local>
2022-07-10 22:25:41 +0530

    Update MailToIntermediary.js
    htmlconvert

--- src/MailToIntermediary.js
+++ src/MailToIntermediary.js
@@ -4,42 +4,56 @@ import React, { useState, useRef } from "react";
 import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import draftToHtml from 'draftjs-to-html';
-import { Button, Grid } from "@mui/material";
+import draftToHtml from "draftjs-to-html";
+import { Button, Grid, TextField, Typography, Paper } from "@mui/material";
 
 function MailToIntermediary() {
   const [editorState, seteditorState] = useState(() =>
     EditorState.createEmpty()
   );
-  const [editorHtml,SetEditorHtml]=useState();
+  const [editorHtml, SetEditorHtml] = useState();
 
   const onEditorStateChange = (editorState) => {
     seteditorState(editorState);
     const currentContent = editorState.getCurrentContent();
     const contentRaw = convertToRaw(currentContent);
-    const value = currentContent.hasText() ? draftToHtml(contentRaw) : '';
+    const value = currentContent.hasText() ? draftToHtml(contentRaw) : "";
     SetEditorHtml(value);
   };
   const editorRef = useRef();
+  const [outputText,setOutputText] = useState();
+
 
   const buttonDefinition = () => {
-      console.log("Editor Content in HTML \n",editorHtml);
+    console.log("Editor Content in HTML \n", editorHtml);
+    setOutputText(editorHtml);
     //   console.log("Editor contetn from ref \n",editorRef.current.editor.editor.innerText);
   };
 
   return (
     <>
-      <Grid>
-        <Editor
-          editorState={editorState}
-          toolbarClassName="toolbarClassName"
-          wrapperClassName="wrapperClassName"
-          editorClassName="editorClassName"
-          onEditorStateChange={onEditorStateChange}
-          ref={editorRef}
-        />
+      <Grid container>
+        <Paper>
+        
+          
+
+            <Grid item xs={6}>
+              <Editor
+                editorState={editorState}
+                toolbarClassName="toolbarClassName"
+                wrapperClassName="wrapperClassName"
+                editorClassName="editorClassName"
+                onEditorStateChange={onEditorStateChange}
+                ref={editorRef}
+              />
+            </Grid>
+    
+          
+        </Paper>
       </Grid>
       <button onClick={buttonDefinition}>Log the editor body</button>
+      {outputText &&  <p>{outputText}</p>
+          }
     </>
   );
 }
